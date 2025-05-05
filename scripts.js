ddec83931222051937e170d624d3d0149d49cb27
hissong <jasonhissong2013@gmail.com>
2025-05-05 12:57:09 -0400

    navigation mobile responsive

--- scripts.js
+++ scripts.js
@@ -2,3 +2,20 @@ function showAnswer(id){
     $("#"+id).toggle();
 }
 
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    if (menu.style.display === 'inline-block') {
+      menu.style.display = 'none';
+    } else {
+      menu.style.display = 'inline-block';
+    }
+  }
+
+  window.addEventListener('resize', () => {
+    const menu = document.getElementById('menu');
+    if (window.innerWidth > 900) {
+      menu.style.display = 'flex';
+    } else {
+      menu.style.display = 'none';
+    }
+  });
