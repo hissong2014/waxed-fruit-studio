d87fd6e27d17a3419411d9b77f734ddb0189243f
hissong <jasonhissong2013@gmail.com>
2025-05-16 11:03:53 -0400

    Added "X Close" change to hamburger menu

--- scripts.js
+++ scripts.js
@@ -6,8 +6,10 @@ function toggleMenu() {
     const menu = document.getElementById('menu');
     if (menu.style.display === 'inline-block') {
       menu.style.display = 'none';
+      document.getElementById("hamburger-menu").innerHTML = "☰ Menu";
     } else {
       menu.style.display = 'inline-block';
+      document.getElementById("hamburger-menu").innerHTML = "X Close";
     }
   }
 
@@ -17,5 +19,6 @@ function toggleMenu() {
       menu.style.display = 'flex';
     } else {
       menu.style.display = 'none';
+      document.getElementById("hamburger-menu").innerHTML = "☰ Menu";
     }
   });
