a2235ab02d6be356d496616d62067c4ead1475ba
hissong <jasonhissong2013@gmail.com>
2025-08-16 19:14:55 -0400

    added cherry logo, corrected menu reponsive design

--- scripts.js
+++ scripts.js
@@ -5,34 +5,46 @@ function showAnswer(id){
 
 function toggleMenu() {
     const menu = document.getElementById('menu');
+
+   
     if (menu.style.display === 'inline-block') {
       menu.style.display = 'none';
       document.getElementById("hamburger-menu").innerHTML = "☰ Menu";
+
+   
     } else {
       menu.style.display = 'inline-block';
       document.getElementById("hamburger-menu").innerHTML = "X Close";
+
+    
     }
   }
 
   window.addEventListener('resize', () => {
     const menu = document.getElementById('menu');
+
     if (window.innerWidth > 900) {
       menu.style.display = 'flex';
+
     } else {
       menu.style.display = 'none';
+      
       document.getElementById("hamburger-menu").innerHTML = "☰ Menu";
     }
   });
 
   $(window).scroll(function () {
     var $heightScrolled = $(window).scrollTop();
+    var windowWidth = $(window).width();
     var $defaultHeight = 50;
 
-    if ($heightScrolled < $defaultHeight) {
-        $('.navigation-image').removeClass("smaller-icon")
+    if (($heightScrolled > $defaultHeight) && (windowWidth > 900))  {
+        $('.navigation-image').addClass("smaller-icon");
+        $('.logo-image').addClass("smaller-icon");
     }
     else {
-        $('.navigation-image').addClass("smaller-icon")
+        $('.navigation-image').removeClass("smaller-icon")
+        $('.logo-image').removeClass("smaller-icon");
     }
 
 });
